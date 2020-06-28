663478cde659b3074bcb7b4bc73861d418629f8c
Andrew Cao <dreamsoftech@users.noreply.github.com>
2020-06-28 09:49:56 -0400

    Update 7-2.js

--- 7-2.js
+++ 7-2.js
@@ -1,32 +1,24 @@
 function solution(A, B) {
-    var upStream = []
-    var downStream = []
-    var N = B.length - 1
+    var stack = []
+    for (var i = 0; i < A.length; i++) {
+        if (stack.length == 0)
+            stack.push(i)
+        else {
+            var dir = B[i]
+            var size = A[i]
+            var k = stack.length - 1
 
-    for (var i = 0; i < B.length/2; i++) {
-        if (B[i] == 0)
-            upStream.push(A[i])
-        else
-            downStream.push(A[i])
-
-        if (i == N-i)
-            break;
-
-        if (B[N-i] == 0)
-            upStream.push(A[N-i])
-        else
-            downStream.push(A[N-i])
+            while(k >= 0) {
+                if ((dir - B[stack[k]]) == -1 && A[stack[k]] < size)
+                    stack.pop()
+                k--
+            }
+            if (stack.length > 0) {
+                if (dir - B[stack[stack.length-1]] != -1)
+                    stack.push(i)
+            } else
+                stack.push(i)
+        }
     }
-    if (upStream.length == 0)
-        return downStream.length
-    if (downStream.length == 0)
-        return upStream.length
-
-    while (upStream.length > 0 && downStream.length > 0) {
-        if (upStream[upStream.length-1] > downStream[downStream.length-1])
-            downStream.pop()
-        else
-            upStream.pop()
-    }
-    return upStream.length + downStream.length
+    return stack.length
 }
